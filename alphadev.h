18aa2729613c1d24e1101480e20d3b9e7d9c041f
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2023-10-30 15:43:50 +0100

    reordered version of alphadev

--- alphadev.h
+++ alphadev.h
@@ -56,3 +56,59 @@ extern "C" void sort3_alphadev(int* buffer) {
       : "eax", "ecx", "edx", "esi", "memory");
 }
 #endif
+
+
+
+#if defined(__clang__)
+
+extern "C" void sort3_alphadev_reorder(int* buffer) {
+  asm volatile(
+      "mov (%0), %%r8d               \n"
+      "mov 0x4(%0), %%eax            \n"
+      "mov 0x8(%0), %%ecx            \n"
+      "cmp %%eax, %%ecx              \n"
+      "mov %%eax, %%edx              \n"
+      "cmovl %%ecx, %%edx            \n"
+      "cmovg %%ecx, %%eax            \n"
+      "cmp %%r8d, %%eax              \n"
+      "mov %%r8d, %%ecx              \n"
+      "cmovl %%eax, %%ecx            \n"
+      "cmovle %%r8d, %%eax           \n"
+      "cmp %%ecx, %%edx              \n"
+      "cmovle %%edx, %%r8d           \n"
+      "cmovg %%edx, %%ecx            \n"
+      "mov %%r8d, (%0)               \n"
+      "mov %%ecx, 0x4(%0)            \n"
+      "mov %%eax, 0x8(%0)            \n"
+      : "+r"(buffer)
+      :
+      : "eax", "ecx", "edx", "r8d", "memory");
+}
+
+#else
+
+// Adapted from [1] for gcc. ("rd8" is replaced with "esi" for the compilation to succeed.)
+extern "C" void sort3_alphadev_reorder(int* buffer) {
+  asm volatile(
+      "mov (%0), %%esi               \n"
+      "mov 0x4(%0), %%eax            \n"
+      "mov 0x8(%0), %%ecx            \n"
+      "cmp %%eax, %%ecx              \n"
+      "mov %%eax, %%edx              \n"
+      "cmovl %%ecx, %%edx            \n"
+      "cmovg %%ecx, %%eax            \n"
+      "cmp %%esi, %%eax              \n"
+      "mov %%esi, %%ecx              \n"
+      "cmovl %%eax, %%ecx            \n"
+      "cmovle %%esi, %%eax           \n"
+      "cmp %%ecx, %%edx              \n"
+      "cmovle %%edx, %%esi           \n"
+      "cmovg %%edx, %%ecx            \n"
+      "mov %%esi, (%0)               \n"
+      "mov %%ecx, 0x4(%0)            \n"
+      "mov %%eax, 0x8(%0)            \n"
+      : "+r"(buffer)
+      :
+      : "eax", "ecx", "edx", "esi", "memory");
+}
+#endif
